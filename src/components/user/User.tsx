776f145301edbe3d61803909e3d0842bbe9f7500
Rafael Padovani <r.padovanni@hotmail.com>
2020-07-08 00:15:27 -0300

    Adding Skeleton Loading with StyledComponents and React Suspense

--- src/components/user/User.tsx
+++ src/components/user/User.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import IUser from '../../interfaces/IUser';
+import SkeletonLoading from '../ui/skeleton-loading/SkeletonLoading';
 
 interface Props {
     user: IUser;
-};
+}
 
 const User: React.FC<Props> = ({ user }) => {
     return (
-        <li>
-            <b>Name:</b> { user.name }
-        </li>
+        <Fragment>
+            <li>
+                <b>Name:</b> {user.name}
+            </li>
+            <li style={{ width: 200, height: 20 }}>
+                <SkeletonLoading />
+            </li>
+            <div
+                style={{
+                    width: 100,
+                    height: 100,
+                    borderRadius: '50%',
+                }}
+            >
+                <SkeletonLoading />
+            </div>
+        </Fragment>
     );
 };
 
