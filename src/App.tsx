5b8c5b3921d28863b2308ea2756626888ec8a81c
Rafael Padovani <r.padovanni@hotmail.com>
2020-07-08 23:49:58 -0300

    Global Styles, Theme and Theme Toggle Test

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import IUser from './interfaces/IUser';
 // STYLES
 import { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from './ui/theme';
+import { GlobalStyles } from './ui/global';
 
 // LAZY IMPORTS
 const UserList = lazy(() => import('./components/user/UserList'));
@@ -37,49 +38,50 @@ const App = () => {
 
     return (
         <ThemeProvider theme={theme}>
-            <header style={{ backgroundColor: theme.backgroundColor }}>
+            <header>
                 <h1 style={{ color: theme.primaryColor }}>Theme</h1>
                 <h2 style={{ color: theme.secondaryColor }}>Toggle Test</h2>
                 <button onClick={onToggleTheme}>TOGGLE THEME</button>
             </header>
 
             <Router>
-                <div className='App'>
-                    <header className='App-header'>
-                        <nav>
-                            <div>
-                                <Link to='/'>Home</Link>
-                            </div>
-
-                            <div>
-                                <Link to='/user'>User</Link>
-                            </div>
-
-                            <div>
-                                <Link to='/user-list'>User List</Link>
-                            </div>
-                        </nav>
-
-                        <br />
-
-                        <SkeletonLoading />
-
-                        <div style={{ width: '500px', height: '50px' }}>
-                            <Suspense fallback={<SkeletonLoading />}>
-                                <Switch>
-                                    <Route path='/user'>
-                                        <User user={user} />
-                                    </Route>
-
-                                    <Route path='/user-list'>
-                                        <UserList />
-                                    </Route>
-                                </Switch>
-                            </Suspense>
+                <header className='App-header'>
+                    <nav>
+                        <div>
+                            <Link to='/'>Home</Link>
                         </div>
-                    </header>
-                </div>
+
+                        <div>
+                            <Link to='/user'>User</Link>
+                        </div>
+
+                        <div>
+                            <Link to='/user-list'>User List</Link>
+                        </div>
+                    </nav>
+
+                    <br />
+
+                    <SkeletonLoading />
+
+                    <div style={{ width: '500px', height: '50px' }}>
+                        <Suspense fallback={<SkeletonLoading />}>
+                            <Switch>
+                                <Route path='/user'>
+                                    <User user={user} />
+                                </Route>
+
+                                <Route path='/user-list'>
+                                    <UserList />
+                                </Route>
+                            </Switch>
+                        </Suspense>
+                    </div>
+                </header>
             </Router>
+
+            {/* Global Styles */}
+            <GlobalStyles />
         </ThemeProvider>
     );
 };
